98f9724124f1288236c035526e32e24e486bed86
Raja Noureddine <raja.noureddine@gmail.com>
2024-07-08 12:05:23 -0400

    Updated instrument generation

--- 12_logit_data_prep/01_data_prep/generate_instruments.py
+++ 12_logit_data_prep/01_data_prep/generate_instruments.py
@@ -11,21 +11,20 @@ import numpy as np
 # Create pd for instruments
 instruments = pd.DataFrame()
 
-if False: # This now works, focus on getting retail density
-    # Import data on charging
-    charging_data = pd.read_csv(data_dir / "charging_stations_output" / "charging_stations_extended.csv")
+# Import data on charging
+charging_data = pd.read_csv(data_dir / "charging_stations_output" / "charging_stations_extended.csv")
 
-    # Get the sum of total, L2, DC in each market and year not for the current county
-    for county in charging_data.county.unique():
-        for year in charging_data.year.unique():
-            other_counties = charging_data.loc[(charging_data.county != county) & (charging_data.year == year)]
-            assert(len(other_counties)==(charging_data.county.nunique()-1))
-            other_counties = pd.DataFrame(other_counties.loc[:, ["county_area_sq_km", "total_stations", "total_stations_L2", "total_stations_DC"]].sum()).T
-            other_counties.columns = ["other_counties_"+x for x in other_counties.columns.tolist()]
-            other_counties["county"] = county
-            other_counties["year"] = year
-            other_counties["market_ids"] = county.upper()+"_"+str(year)
-            instruments = pd.concat([instruments, other_counties])
+# Get the sum of total, L2, DC in each market and year not for the current county
+for county in charging_data.county.unique():
+    for year in charging_data.year.unique():
+        other_counties = charging_data.loc[(charging_data.county != county) & (charging_data.year == year)]
+        assert(len(other_counties)==(charging_data.county.nunique()-1))
+        other_counties = pd.DataFrame(other_counties.loc[:, ["county_area_sq_km", "total_stations", "total_stations_L2", "total_stations_DC"]].sum()).T
+        other_counties.columns = ["other_counties_"+x for x in other_counties.columns.tolist()]
+        other_counties["county"] = county
+        other_counties["year"] = year
+        other_counties["market_ids"] = county.upper()+"_"+str(year)
+        instruments = pd.concat([instruments, other_counties])
 
 # Now for each county, get the density of retail establishments... harder.
 advan_data = pd.read_csv(data_dir / "advan_data_footfall_processed" / "footfall_CT_2020-01-07.csv")
@@ -59,4 +58,15 @@ advan_data = advan_data[(advan_data.raw_visitor_counts > advan_data.pctile_05) &
 
 # Now get the count of establishments per county
 retail_per_county = advan_data.value_counts("county").reset_index()
-retail_per_county["county"] = retail_per_county["county"].str.upper()
+retail_per_county["county"] = retail_per_county["county"].str.lower()
+retail_per_county = retail_per_county.merge(charging_data[["county", "county_area_sq_km"]].drop_duplicates(), on = "county", how = "left")
+retail_per_county["retail_density_2020"] = retail_per_county["count"] /  retail_per_county["county_area_sq_km"] / 1000
+
+# Now get the instrument
+instruments = instruments.merge(retail_per_county[["county", "retail_density_2020"]], on = "county")
+instruments["bartik_instrument_L2"] = instruments["other_counties_total_stations_L2"] * instruments["retail_density_2020"]
+instruments["bartik_instrument_DC"] = instruments["other_counties_total_stations_DC"] * instruments["retail_density_2020"]
+instruments["bartik_instrument_total"] = instruments["other_counties_total_stations"] * instruments["retail_density_2020"]
+
+# Now save
+instruments.to_csv(data_dir / "instruments" / "instruments.csv")
